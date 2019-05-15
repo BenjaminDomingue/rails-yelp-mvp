427fb8961a71c436f0d3b96bb20353f540aa781e
Benjamin Domingue <benjamin.domingue1991@gmail.com>
2019-05-15 15:13:22 -0400

    yelp avance

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -9,7 +9,7 @@ class ReviewsController < ApplicationController
     @review = Review.new(review_params)
     @review.restaurant = Restaurant.find(params[:restaurant_id])
     if @review.save
-      redirect_to restaurant_path(@review)
+      redirect_to restaurant_path(Restaurant.find(params[:restaurant_id]))
     else
       render :new
     end
